2d4c9a34626dd56b2501fa0c4e73cccfeb8b238e
mvirk4 <mvirk@flemingcollege.ca>
2024-02-01 12:28:24 -0500

    Split the sales data into individual orders and save to Excel sheets

--- Lab3.py
+++ Lab3.py
@@ -26,16 +26,38 @@ def get_sales_csv():
 # Create the directory to hold the individual order Excel sheets
 def create_orders_dir(sales_csv):
     # Get directory in which sales data CSV file resides
+    sales_csv_path = os.path.abspath()(sales_csv)
+    sales_csv_dir = os.path.dirname(sales_csv_path)
+
     # Determine the name and path of the directory to hold the order data files
+    current_date = date.today().isoformat()
+    folder_name = f"orders_{current_date}"
+    order_dir = os.path.join(sales_csv_dir, folder_name)
+
     # Create the order directory if it does not already exist
-    return 
+    if not os.path.isdir(order_dir):
+        os.mkdir(order_dir)
+
+    return order_dir
 
 # Split the sales data into individual orders and save to Excel sheets
 def process_sales_data(sales_csv, orders_dir):
     # Import the sales data from the CSV file into a DataFrame
+    sales_df = pd.read_csv(sales_csv)
     # Insert a new "TOTAL PRICE" column into the DataFrame
+    sales_df.insert(7, "TOTAL PRICE", sales_df["QUANTITY"] * sales_df["PRICE"])
+    
     # Remove columns from the DataFrame that are not needed
+    sales_df.drop(columns =["ADDRESS","CITY","STATE","POSTAL CODE","COUNTRY"], inplace = True)
     # Group the rows in the DataFrame by order ID
+    grouped_df = sales_df.groupby("ORDER ID")
+
+    # For each order ID:
+    
+        # Remove the "ORDER ID" column
+        # Sort the items by item number
+        # Append a "GRAND TOTAL" row
+        # Determine the file name and full path of the Excel sheet
     # For each order ID:
         # Remove the "ORDER ID" column
         # Sort the items by item number
